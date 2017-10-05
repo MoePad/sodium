c5193fe99f6eff64d5c59b561c79603f37a981a7
MoePad <moepad380@googlemail.com>
2017-10-05 23:43:41 +0200

    added dynamic list example

---
+++ book/swidgets/java/examples/dynamic_list.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import java.awt.FlowLayout;
+import swidgets.*;
+import nz.sodium.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class dynamic_list {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Dynamic List");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        Transaction.runVoid(() -> {
+          SButton addBtn = new SButton("Add");
+          SButton rmBtn = new SButton("Remove");
+          STextField input = new STextField("");
+
+
+          CellLoop<Cell<List<String>>> dynamicList = new CellLoop<>();
+
+          Stream<String> sAdd = addBtn.sClicked.snapshot(input.text, (u, t) -> t);
+
+          Stream<Cell<List<String>>> sList = sAdd.snapshot(dynamicList,
+                    (text, cellList) -> {
+                      List<String> currentList = cellList.sample();
+                      currentList.add(text);
+                      return new Cell<List<String>>(currentList);
+                    });
+
+          Cell<String> currentValue = Cell.switchC(dynamicList).map(list -> {
+              String returnString = "";
+              for(String s : list) {
+                returnString += s + " ";
+              }
+              return returnString;
+            });
+          dynamicList.loop(sList.hold(new Cell<List<String>>(new ArrayList<>())));
+
+          SLabel lblSelection = new SLabel(currentValue);
+
+          frame.add(rmBtn);
+          frame.add(addBtn);
+          frame.add(input);
+          frame.add(lblSelection);
+        });
+
+        frame.setSize(400, 160);
+        frame.setVisible(true);
+    }
+
+    static <A> Cell<List<A>> sequence(Collection<Cell<A>> in) {
+        Cell<List<A>> out = new Cell<>(new ArrayList<A>());
+        for (Cell<A> c : in)
+            out = out.lift(c,
+                (list0, a) -> {
+                    List<A> list = new ArrayList<A>(list0);
+                    list.add(a);
+                    return list;
+                });
+        return out;
+    }
+
+    private static Integer parseInt(String t) {
+        try {
+            return Integer.parseInt(t);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}
