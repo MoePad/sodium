4a003aff6c5142f86a594c8421551b4865d61455
MoePad <moepad380@googlemail.com>
2017-10-14 15:57:46 +0200

    added continuous time example 1

---
+++ book/swidgets/java/examples/continuous_time_1.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.FlowLayout;
+import swidgets.*;
+import nz.sodium.*;
+import nz.sodium.time.*;
+import java.util.*;
+
+public class continuous_time_1 {
+    public static void main(String[] args) throws InterruptedException {
+        setUpLogic();
+        loop();
+    }
+
+    private static void setUpLogic() {
+        JFrame frame = new JFrame("Continuous Time 1");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        TimerSystem timerSystem = new SecondsTimerSystem();
+        Cell<Double> time = timerSystem.time;
+
+        SLabel lblValue = new SLabel(time.map(value -> Double.toString(value)));
+
+        frame.add(lblValue);
+        frame.setSize(400, 160);
+        frame.setVisible(true);
+    }
+
+    private static void loop() throws InterruptedException {
+        long systemSampleRate = 1000L;
+        while(true) {
+            //jede Transaktion aktualisert TimerSystem.time
+            Transaction.runVoid(() -> {});
+            Thread.sleep(systemSampleRate);
+        }
+    }
+}
